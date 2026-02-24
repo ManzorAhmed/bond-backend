ce155108cbe1282cbe66db03727dae208f7ac18e
Manzoor Ahmed <manzoor.yaqoob625@gmail.com>
2026-02-24 16:15:44 +0400

    fix db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,35 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BondModule } from './bond/bond.module';
-import { Bond } from './bond/entities/bond.entity';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { BondModule } from "./bond/bond.module";
+import { Bond } from "./bond/entities/bond.entity";
 
 @Module({
   imports: [
-    
     ConfigModule.forRoot({
       isGlobal: true,
     }),
 
     // MySQL connection via TypeORM
    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: "postgres",
-        url: config.get<string>("DATABASE_URL"),
+  imports: [ConfigModule],
+  inject: [ConfigService],
 
-        autoLoadEntities: true,
-        synchronize: false,
+  useFactory: (config: ConfigService) => ({
+    type: "postgres",
 
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }),
-    }),
+    url: config.get<string>("DATABASE_URL"),
 
+    autoLoadEntities: true,
+
+    synchronize: false,
+
+    ssl:
+      config.get("NODE_ENV") === "production"
+        ? { rejectUnauthorized: false }
+        : false,
+  }),
+}),
 
     // Feature module
     BondModule,
